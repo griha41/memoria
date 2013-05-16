87c226a83cce5712ce2d0cf3d160017b61fc76e7
Gerardo Villarroel <gvillarroel@bazuca.com>
2013-05-15 21:56:57 -0400

    se remueve elemento intermediario container

--- rec.note/RecNote/RecNote.Data.MongoDB/Container.cs
+++ rec.note/RecNote/RecNote.Data.MongoDB/Container.cs
@@ -7,17 +7,28 @@ using MongoDB.Bson;
 
 namespace RecNote.Data.MongoDB
 {
-    public class ContainerS<T> where T : Entities.Base
+    public class Container<T> where T : Entities.Base
     {
         public ObjectId Id { get; set; }
         public T Object { get; set; }
 
-        public ContainerS(T obj)
+        public Container(T obj)
         {
             this.Object = obj;
         }
 
-        
+        public static implicit operator T(Container<T> obj)
+        {
+            if (obj == null) return null;
+            var e = obj.Object;
+            e.ID = obj.Id.ToString();
+            return e;
+        }
+
+        public static implicit operator Container<T>(T obj)
+        {
+            return new Container<T>(obj);
+        }
 
     }
 }
