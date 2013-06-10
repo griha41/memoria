b1fa3e52c3185169d9d846ff82c56d5420a5df05
Gerardo Villarroel <gvillarroel@bazuca.com>
2013-06-10 20:04:14 -0300

    terminado comentarios, pendiente respuestas

---
+++ rec.note/RecNote/RecNote.Entities/Projects/ProjectItemComment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RecNote.Entities.Projects
+{
+    public class ProjectItemComment
+    {
+        public string UserID { get; set; }
+        public string Message { get; set; }
+        [MongoDB.Bson.Serialization.Attributes.BsonId]
+        public DateTime Time { get; set; }
+    }
+}
---
+++ rec.note/RecNote/RecNote.Presentation.Web/Models/ProjectItem/ViewComment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RecNote.Presentation.Web.Models.ProjectItem
+{
+    public class ViewComment : View
+    {
+        public RecNote.Entities.Projects.ProjectItemComment Comment { get; set; }
+        public RecNote.Entities.Projects.Actor[] Actors { get; set; }
+        public RecNote.Entities.Projects.Actor Actor { get; set; }
+    }
+}
